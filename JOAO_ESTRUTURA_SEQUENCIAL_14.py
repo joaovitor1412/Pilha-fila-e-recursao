efe80014eaa97abf4851d752a59b01a0b02257f8
joaovitor1412 <joao_vp_goedert@estudante.sesisenai.org.br>
2025-10-28 20:45:11 -0300

    Create JOAO_ESTRUTURA_SEQUENCIAL_14.py

---
+++ JOAO_ESTRUTURA_SEQUENCIAL_14.py
@@ -0,0 +1,7 @@
+#Faça um programa que peça as 4 notas bimestrais e mostre a média.
+número_1 =float(input("escreva número_1: "))
+número_2 = float(input("escreva número_2: "))
+número_3 = float(input("escreva número_3: "))
+número_4 = float(input("escreva número_4: "))
+media = (número_1 + número_2 + número_3 + número_4) / 4
+print(f"media {media}")
