5097c85e89a025e60183a67876018860dfb93832
joaovitor1412 <joao_vp_goedert@estudante.sesisenai.org.br>
2025-10-28 20:46:47 -0300

    Create JOAO_ESTRUTURA_SEQUENCIAL_15.py

---
+++ JOAO_ESTRUTURA_SEQUENCIAL_15.py
@@ -0,0 +1,4 @@
+#Faça um programa que converta metros para centímetros:
+metros = float(input("escreva os metros"))
+centimetros = metros * 100
+print(f"metros{metros} em metros {centimetros} em centimetros.")
